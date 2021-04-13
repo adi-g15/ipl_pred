05733924095c9be7e2236ddb426fc26504c5027b
Aditya Gupta <ag15035@gmail.com>
2021-04-13 16:17:10 +0530

    Scraping data from slider

--- functions/list_remain.js
+++ functions/list_remain.js
@@ -1,6 +1,30 @@
 const fetch = require("node-fetch");
 const $ = require("cheerio");
 
+function way_2() {
+	const matches = [];
+	const document = $.load("");
+	const date_nodes = document.querySelectorAll(".match-scroller__date");
+	const nodes = document.querySelectorAll(".match-scroller__item");
+	nodes.forEach((outer_node, i) => {
+		const node = outer_node.querySelector(".match-item__content");
+		const venue = outer_node.querySelector(".match-item__venue")
+		const res = outer_node.querySelector(".match-item__summary")
+		matches.push({
+			'0': node.querySelector('.match-item__team--a .match-item__team-name').innerText,
+			'1': node.querySelector('.match-item__team--b .match-item__team-name').innerText,
+			res,
+			venue,
+			date: [
+				date_nodes[i].querySelector('.match-scroller__day').innerText
+				date_nodes[i].querySelector('.match-scroller__month').innerText
+			]
+		});
+	});
+
+	return matches;
+}
+
 exports.handler = async (event, context) => {
     try{
         const html = await fetch("https://www.iplt20.com/matches/schedule/men")
