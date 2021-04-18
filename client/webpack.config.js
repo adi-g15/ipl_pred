96ef44656759bdb2808e4234e86fa59e4baeb248
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-04-19 01:54:16 +0530

    Test deploy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,9 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
+// @adi-g
+// https://lannonbr.com/blog/2020-02-17-wasm-pack-webpack-plugin
+// https://dev.to/lokesh007/webassembly-with-rust-and-react-using-create-react-app-67
 module.exports = {
   entry: "./bootstrap.js",
   output: {
