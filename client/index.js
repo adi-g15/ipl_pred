d0845eca58e7426be367ff75b26b5d0f01c7f197
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-04-19 02:42:36 +0530

    f5

--- client/index.js
+++ client/index.js
@@ -1,11 +1,25 @@
 import * as wasm from "ipl_pred"
 
 function handleClick(data) {
+    const possibilities = wasm.get_chances(JSON.stringify(data));
+
 
 }
 
 async function fetchData() {
-    return fetch("/")
+    return fetch("/.netlify/functions/league_matches")
+            .then(res => res.json())
+            .then(matches => {
+                if(!Array.isArray(matches)) throw new Error("Received JSON is not an Array");
+
+                return matches.map(match => ({  // we don't want the venue and date fields
+                    '0': match['0'],
+                    '1': match['1'],
+                    res: match['res']
+                }));
+            })
 }
 
-handleClick( await fetchData() );
+fetchData()
+    .then(json_obj => handleClick(json_obj))
+    .catch(console.error)
