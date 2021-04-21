e110f51f3cfee6ffc7e91ead870c6033c3f5308d
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-04-21 23:27:05 +0530

    Automatic Data Update 21-04-2021

--- data_updator.cmd
+++ data_updator.cmd
@@ -1,3 +1,3 @@
 @echo on
 
-yarn run sync_data && git add . && git commit -m "Data Update %date%" && git push origin main
+yarn run sync_data && git add . && git commit -m "Automatic Data Update %date%" && git push origin main
